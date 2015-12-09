b5ec08d5f1bed272e4fda12109efec8c8232b1a9
igor <igor.alvarezsuarez@mecalux.com>
2015-12-09 08:17:52 +0100

    Hola mundo

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("hola mundo");
         }
     }
 }
